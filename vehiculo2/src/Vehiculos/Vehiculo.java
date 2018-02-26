c97bc68bb6407e5ab534c9e5f86d6acae0bc9b58
Estudiantes <Estudiantes@Labing70207>
2018-02-26 17:45:05 -0500

    parqueadero version 2.2

---
+++ vehiculo2/src/Vehiculos/Vehiculo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vehiculos;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Vehiculo {
+    
+        public String marca;
+        public int cilindraje;
+        public String modelo;
+        public int año;
+        
+        void acelerar(){
+        
+        
+         }
+         void frenar(){
+        
+         }
+         
+}
+
